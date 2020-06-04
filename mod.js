7e5b0ac3f732b00e0e7c74eeaf837c07d3419011
L Sherry <lesser.sherry@protonmail.com>
2020-06-04 21:22:11 +0200

    Try to modernize this stuff.
    Use cool new keywords like 'const' which declares unseatable references
    to non-const data. And use a consistent style.
    
    Also add a fancy .eslintrc.json to make the project more hype and cool,
    instead of boring files such as CHANGES or Documentation.

--- mod.js
+++ mod.js
@@ -1,28 +1,27 @@
-var normal_nbsp = '\u00a0';
-var normal_shorter_nbsp = '\u202f';
 // This is normally U+00A0, except the game uses /\s/ to check for
 // space to break, and it matches, so it would breaks our non-breaks.
-var our_nbsp = '\u0080';
+const our_nbsp = "\u0080";
 // This would be U+202F or something.
-var shorter_nbsp = '\u0081';
+const shorter_nbsp = "\u0081";
 
 // create a replace func given a single-character => replacement map.
-var map_to_replace_func = map => {
-	var regex = new RegExp('[' + Object.keys(map).join('') + ']', 'g');
+const map_to_replace_func = map => {
+	const regex = new RegExp("[" + Object.keys(map).join("") + "]", "g");
 	return text => text.replace(regex, character => map[character]);
 };
 
 // turn normal nbsp to our nbsp replacements.
-var filter_normal_nbsps = map_to_replace_func({
-	'\u00a0': our_nbsp,
-	'\u202f': shorter_nbsp
+const filter_normal_nbsps = map_to_replace_func({
+	// normal nbsp
+	"\u00a0": our_nbsp,
+	"\u202f": shorter_nbsp
 });
 
-// The 1990 called, there're telling us that our 'é' is a copyrighted
+// The 1990 called, there're telling us that our "é" is a copyrighted
 // latin capital letter A with tilde. We told them to use utf8, but
 // they don't seem to understand. They think javascript source code
 // must be encoded in latin9 or something.
-var c = {
+const c = {
 	"°":"\u00b0",
 	"à":"\u00e0", "á":"\u00e1", "â":"\u00e2", "ä":"\u00e4",
 	"À":"\u00c0", "Á":"\u00c1", "Â":"\u00c2", "Ä":"\u00c4",
@@ -36,32 +35,32 @@ var c = {
 	"Œ":"\u0152", "œ":"\u0153",
 };
 
-var text_filter = (text, result) => {
+const text_filter = (text, result) => {
 	// nbsp.
-	text = text.replace(/ ([:!?])/g, our_nbsp+'$1');
+	text = text.replace(/ ([:!?])/g, our_nbsp+"$1");
 	// normally this should be done every time, but there are some
 	// degenerate texts abusing it, so only do it at start of line.
-	text = text.replace(/^\.\.\. /g, '...'+our_nbsp);
+	text = text.replace(/^\.\.\. /g, "..."+our_nbsp);
 	// shorter nbsp
-	text = text.replace(/ ([;%])/g, shorter_nbsp+'$1');
+	text = text.replace(/ ([;%])/g, shorter_nbsp+"$1");
 	// there is sometimes the need to truly encode a nbsp,
 	// e.g. in the insult generator, because the game will
-	// internally append '!' at the end.
+	// internally append "!" at the end.
 	text = filter_normal_nbsps(text);
 
 	if (result.quality)
-		text += '(' + result.quality + ')';
+		text += `(${result.quality})`;
 	return text;
 };
 
-// font patching: make a 'î' out of a 'â' and 'i'
-// font patching: make a 'ï' out of a 'ë' and 'i'
-// font patching: make a 'i' with an accent picked from from_e_char
-var figure_out_accented_i_patch = (font_context, from_e_char, to_i_char) => {
-	var trem_rect;
-	var base_rect = font_context.get_char_pos(from_e_char);
-	var shift_i_x = 0;
-	var shift_trem = { x: 0, y: 0};
+// font patching: make a "î" out of a "â" and "i"
+// font patching: make a "ï" out of a "ë" and "i"
+// font patching: make a "i" with an accent picked from from_e_char
+const figure_out_accented_i_patch = (font_context, from_e_char, to_i_char) => {
+	let trem_rect;
+	const base_rect = font_context.get_char_pos(from_e_char);
+	let shift_i_x = 0;
+	let shift_trem = { x: 0, y: 0};
 	switch (base_rect.height) {
 	case 7:
 		trem_rect = { x: 1, y: 0, width: 3, height: 2 };
@@ -78,8 +77,8 @@ var figure_out_accented_i_patch = (font_context, from_e_char, to_i_char) => {
 		break;
 	}
 
-	var dest_rect = font_context.get_char_pos(to_i_char);
-	var i_src = font_context.get_char_pos('i');
+	const dest_rect = font_context.get_char_pos(to_i_char);
+	const i_src = font_context.get_char_pos("i");
 	dest_rect.width = Math.max(trem_rect.width, i_src.width);
 	// must fill this blank
 	font_context.clears.push(dest_rect);
@@ -94,7 +93,7 @@ var figure_out_accented_i_patch = (font_context, from_e_char, to_i_char) => {
 	});
 
 	// put trems here
-	var trems_dst = {
+	const trems_dst = {
 		x: dest_rect.x + trem_rect.x + shift_trem.x,
 		y: dest_rect.y + trem_rect.y + shift_trem.y,
 		width: trem_rect.width,
@@ -110,30 +109,30 @@ var figure_out_accented_i_patch = (font_context, from_e_char, to_i_char) => {
 };
 
 // font patching: fix "éèê" which are horrible.
-var figure_out_e_patch = font_context => {
-	var crop_y = (c, y, shift_y) => {
-		var rect = font_context.get_char_pos(c);
+const figure_out_e_patch = font_context => {
+	const crop_y = (c, y, shift_y) => {
+		const rect = font_context.get_char_pos(c);
 		rect.height = rect.height - Math.abs(y);
 		rect.y += Math.max(y,0) + (shift_y || 0);
 		return rect;
 	};
-	var blits = [
-		{ from: crop_y('e', 5), to: crop_y(c['é'], 5)},
-		{ from: crop_y('e', 5), to: crop_y(c['è'], 5)},
-		{ from: crop_y('e', 5), to: crop_y(c['ê'], 5)},
-		{ from: crop_y('e', 5), to: crop_y(c['ë'], 5)},
-		{ from: crop_y(c['à'], -8), to: crop_y(c['è'], -8)},
-		{ from: crop_y(c['á'], -8), to: crop_y(c['é'], -8)},
-		{ from: crop_y(c['Â'], -9), to: crop_y(c['ê'], -9, 1)},
-		{ from: crop_y(c['ä'], -8), to: crop_y(c['ë'], -8)}
+	const blits = [
+		{ from: crop_y("e", 5), to: crop_y(c["é"], 5)},
+		{ from: crop_y("e", 5), to: crop_y(c["è"], 5)},
+		{ from: crop_y("e", 5), to: crop_y(c["ê"], 5)},
+		{ from: crop_y("e", 5), to: crop_y(c["ë"], 5)},
+		{ from: crop_y(c["à"], -8), to: crop_y(c["è"], -8)},
+		{ from: crop_y(c["á"], -8), to: crop_y(c["é"], -8)},
+		{ from: crop_y(c["Â"], -9), to: crop_y(c["ê"], -9, 1)},
+		{ from: crop_y(c["ä"], -8), to: crop_y(c["ë"], -8)}
 	];
 	blits.forEach(v => v.from.width = v.to.width);
 	font_context.blits = font_context.blits.concat(blits);
 };
 
-var patch_oe = (image, font_context) => {
-	for (const chars of [{o:'o', e:'e', oe:c['œ']},
-			     {o:'O', e:'E', oe:c['Œ']}]) {
+const patch_oe = (image, font_context) => {
+	for (const chars of [{o:"o", e:"e", oe:c["œ"]},
+			     {o:"O", e:"E", oe:c["Œ"]}]) {
 		const o = font_context.get_char_pos(chars.o);
 		const e = font_context.get_char_pos(chars.e);
 
@@ -158,8 +157,8 @@ var patch_oe = (image, font_context) => {
 	}
 };
 // Patch the font metrics and figure out how to patch the font.
-var handle_metrics = (image, font_context) => {
-	var space = font_context.get_char_pos(' ');
+const handle_metrics = (image, font_context) => {
+	const space = font_context.get_char_pos(" ");
 	font_context.set_char_pos(our_nbsp, space);
 	// Normally, we would turn a quarter em into a tens of em.
 	// Except it would be too short here, not even enough for
@@ -176,17 +175,17 @@ var handle_metrics = (image, font_context) => {
 		// The degree sign from the font looks like an upper
 		// dot.  Replace it by a MASCULINE ORDINAL INDICATOR
 		// which, in the font, looks more like a degree.
-		var degree = font_context.get_char_pos('\u00ba');
-		font_context.set_char_pos(c['°'], degree);
+		const degree = font_context.get_char_pos("\u00ba");
+		font_context.set_char_pos(c["°"], degree);
 	}
 	// Font patching: add î and ï out of ê and ë
-	figure_out_accented_i_patch(font_context, c['ë'], c['ï']);
-	figure_out_accented_i_patch(font_context, c['ê'], c['î']);
+	figure_out_accented_i_patch(font_context, c["ë"], c["ï"]);
+	figure_out_accented_i_patch(font_context, c["ê"], c["î"]);
 	patch_oe(image, font_context);
 };
 // Patch the given font.  Easier than shipping 16 modified pngs.
-var patch_font = (image, font_context) => {
-	var context = image.getContext("2d");
+const patch_font = (image, font_context) => {
+	const context = image.getContext("2d");
 	context.imageSmoothingEnabled = false;
 	font_context.clears.forEach(v => {
 		context.clearRect(v.x, v.y, v.width, v.height);
@@ -204,16 +203,16 @@ var patch_font = (image, font_context) => {
 	});
 	return image;
 };
-var prepare_patch_font = (image, font_context) => {
+const prepare_patch_font = (image, font_context) => {
 	handle_metrics(image, font_context);
 	return patch_font(image, font_context);
-}
+};
 
 // Format a number.
-var format_number = (number, precision, unit, template) => {
+const format_number = (number, precision, unit, template) => {
 	// toLocaleString("fr-FR") uses nbsp of course, so patch them too.
 	template = filter_normal_nbsps(template);
-	if (unit && unit != "%")
+	if (unit && unit !== "%")
 		template = (
 			template.slice(0, -unit.length)
 			+ our_nbsp + template.slice(-unit.length)
@@ -224,17 +223,17 @@ var format_number = (number, precision, unit, template) => {
 // ccloader versions before 2.11.0 do not support ccmodDependencies,
 // which means that localize-me may or may not be loaded first (if at
 // all), depending on directory listing order which is quite fragile.
-var is_very_old_ccloader = () => {
+const is_very_old_ccloader = () => {
 	if (!(window.activeMods && window.inactiveMods))
 		return false;
 
-	var all_mods = window.activeMods.concat(window.inactiveMods);
-	var simplify = all_mods.find(({name}) => name === "Simplify");
+	const all_mods = window.activeMods.concat(window.inactiveMods);
+	const simplify = all_mods.find(({name}) => name === "Simplify");
 	if (!simplify)
 		return false;
 
 	// hack to use lexicographical compare for semvers
-	var version = simplify.version.replace(/[0-9]+/g, m =>
+	const version = simplify.version.replace(/[0-9]+/g, m =>
 		String.fromCharCode(Math.min(Number.parseInt(m, 10), 127)));
 
 	// ccloader 2.11.0 ships with simplify 2.3.3
@@ -249,8 +248,8 @@ if (is_very_old_ccloader()) {
 			      "above to work properly.");
 		console.error("Votre version de CCLoader est bien " +
 			      "trop ancienne ! French-CC "+
-			      "n" + c['é'] + "cessite une version " +
-			      "2.11.0 ou ult" + c['é'] + "rieure " +
+			      "n" + c["é"] + "cessite une version " +
+			      "2.11.0 ou ult" + c["é"] + "rieure " +
 			      "pour fonctionner correctement.");
 	}, 5000);
 	throw new Error("Your CCLoader version is way too old! " +
@@ -258,20 +257,20 @@ if (is_very_old_ccloader()) {
 			"above to work properly.");
 }
 
-var my_prefix = import.meta.url.slice(0, -"mod.js".length);
+const my_prefix = import.meta.url.slice(0, -"mod.js".length);
 window.localizeMe.add_locale("fr_FR", {
 	from_locale:"en_US",
 	map_file: my_prefix + "map_file.json",
 	url_prefix: my_prefix,
 	language: {
 		en_US: "French",
-		de_DE: "Franz" + c['ö'] + "sisch",
-		fr_FR: "Fran" + c['ç'] + "ais",
+		de_DE: "Franz" + c["ö"] + "sisch",
+		fr_FR: "Fran" + c["ç"] + "ais",
 	},
 	text_filter: text_filter,
 	patch_base_font: prepare_patch_font,
 	patch_font: patch_font,
-	number_locale: 'fr-FR',
+	number_locale: "fr-FR",
 	format_number: format_number,
 	misc_time_function: () => {
 		const date = new Date();
