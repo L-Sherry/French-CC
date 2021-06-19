3c5bebe0affb61b3721f1d6bf2f0901591278e1b
L Sherry <lesser.sherry@protonmail.com>
2021-06-19 18:27:28 +0200

    Add a small github action to push ccmod and easy-installers.
    easy-installers are basically archives that you can extract into your
    CrossCode installation directory to install both a modloader and the
    required mods.
    
    This add a simple github workflow that uses a small posix shell script
    (no need to make things complicated) to create a ccmod and two easy
    installers: one that uses CCLoader v2 and one that uses ccloader3.
    I would gladly explain why, but i can't talk about politics because the
    polls open tomorrow.  What a shame.

---
+++ .github/workflows/release.sh
@@ -0,0 +1,63 @@
+#!/bin/sh -eu
+# usage: $0 repository-name tag-ref-name
+EMPTY=4b825dc642cb6eb9a060e54bf8d69288fbee4904
+BASENAME="${1##*/}-${2##*/}"
+ccmod="$BASENAME.ccmod"
+quickinstall2="$BASENAME-quick-install-ccloader2.zip"
+quickinstall3="$BASENAME-quick-install-ccloader3.zip"
+
+# $1: output $2: tree (if unset, read tree from stdin)
+make_reproducible_zip() {
+	git cat-file -p "${BASENAME##*-}" \
+		| sed -e "1s-[^ ]*\$-${2-$(cat <&5)}-" -e 2s-c.\*-tree- -e 5q \
+		| git hash-object -t tag -w --stdin \
+		| xargs git archive -9 --format=zip -o "$1"
+}
+# $1:urlbase $2:urlfile $3:sha256 $4:dirbase $5:components $6:optional output
+fetch_extract_add_and_optionally_create_quick_installer_quickly() {
+	if [ ! -s "$4-src-$3.tar.gz" ]; then
+		${WGET:-wget} --timeout=60 "$1/$2" -O "$3"
+		sha256sum -c <<EOF >/dev/null
+$3  $3
+EOF
+		mv "$3" "$4-src-$3.tar.gz"
+	fi | rm -rf "$4-src" | xargs mkdir "$4-src"
+	tar -C "$4-src" --strip-components="$5" -xzf "$4-src-$3.tar.gz"
+	export GIT_INDEX_FILE="$4-src/index"
+	git add -f "$4-src"/*
+	${6+: --check-error} return
+	GIT_INDEX_FILE="$4-src/nothing" xargs git read-tree \
+		--index-output "$GIT_INDEX_FILE" -i -m \
+		"$EMPTY" "$(git write-tree --prefix="$4-src/")"
+	git write-tree | make_reproducible_zip "$6" 5<&0
+	rm -rf "$4-src"
+}
+mkfifo fifo3 fifo2
+fetch_extract_add_and_optionally_create_quick_installer_quickly \
+	https://stronghold.crosscode.ru/~dmitmel/ccloader3/20210530154955 \
+	ccloader_3.0.0-alpha_quick-install.tar.gz \
+	8adde9fbe5db9ada895bc1d8a59b327af1e8f07710a9f84960ada75b0ddd654d \
+	ccloader3 0 "$quickinstall3" < fifo3 & CCLOADER3_PID=$!
+fetch_extract_add_and_optionally_create_quick_installer_quickly \
+	https://github.com/CCDirectLink/CCLoader/archive/refs/tags/v2.20.2 \
+	v2.10.1.tar.gz \
+	b8de00b24386ae26b53ac842ae7fb3a7bc49142ebf5d8c4be15610cd6adde812 \
+	ccloader2 1 "$quickinstall2" < fifo2 & CCLOADER2_PID=$!
+exec 8> fifo3 9> fifo2
+rm -rf fifo2 fifo3
+
+TREE="$(git cat-file -p "$2^{tree}" | grep -Ev '\s[MR.]|^1[^0]' | git mktree)"
+make_reproducible_zip "$BASENAME.ccmod" "$TREE" & CCMOD_PID=$!
+fetch_extract_add_and_optionally_create_quick_installer_quickly \
+	https://github.com/L-Sherry/Localize-me/archive/cd84932c815297c6777fa \
+	localize-me-v2.5-2026-02-15-via-ipot.tar.gz \
+	1e662fd268ebbac12ad5edd84843e2fc00976043ca453709f80d6b4fd31f791c \
+	localizeme 1
+printf %s "$EMPTY $EMPTY -i --prefix=assets/mods $(git mktree << MODS
+040000 tree $TREE	French
+040000 tree $(git write-tree --prefix localizeme-src)	Localize-Me-1.x
+MODS
+)" | xargs -s 97 git read-tree | xargs git write-tree | tee /dev/fd/8 >&9
+exec 8>&- 9>&-
+rm -rf localizeme-src
+for PID in $CCMOD_PID $CCLOADER2_PID $CCLOADER3_PID; do wait $PID; done
